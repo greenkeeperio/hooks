afa0d9d0dc2180ec2fe19fea86c994ef72280d53
Julia Krüger <juliakbln@gmail.com>
2017-10-02 14:01:27 +0200

    test(npm-event): fix tests for large files

--- test/npm-event.js
+++ test/npm-event.js
@@ -96,17 +96,15 @@ const register = require('../lib/npm-event')
   })
 
   tap.test('accepts packages that are bigger then 1MB', async (t) => {
-    const bigBody = Buffer.alloc(1024 * 1024 * 5).toString()
-    const reqPayload = JSON.stringify({payload: {
-      name: '@test/test',
-      'dist-tags': {
-        latest: '1.0.0'
-      },
-      versions: {
-        '1.0.0': {}
-      },
-      body: bigBody
-    }})
+    const bigBody = Buffer.alloc(1024 * 1024 * 3).toString()
+    const reqPayload = JSON.stringify({
+      payload: {
+        body: bigBody
+      }
+    })
+    // Because of the JSON.stringify() the payload with the 3MB Buffer actually
+    // has a size of ~18MB
+    const payloadSize = reqPayload.length
 
     server.register({
       register,
@@ -128,12 +126,54 @@ const register = require('../lib/npm-event')
       headers: {
         'x-npm-signature': `sha256=${hmacPayload}`,
         'Content-Type': 'application/json',
-        'Content-Length': 5 * 1024 * 1024 // 5 MB
+        'Content-Length': payloadSize
+      },
+      payload: reqPayload
+    })
+    t.true((payloadSize > 1024 * 1024 * 1), 'payload is bigger then 1MB')
+    t.true((payloadSize < 1024 * 1024 * 25), 'payload is smaller then 25MB')
+    t.is(statusCode, 202, 'statusCode')
+    t.end()
+  })
+
+  tap.test('does not accept packages that are bigger then 25MB', async (t) => {
+    const bigBody = Buffer.alloc(1024 * 1024 * 5).toString()
+    const reqPayload = JSON.stringify({
+      payload: {
+        body: bigBody
+      }
+    })
+    // Because of the JSON.stringify() the payload with the 5MB Buffer actually
+    // has a size of ~30MB
+    const payloadSize = reqPayload.length
+
+    server.register({
+      register,
+      options: {env, channel}
+    })
+
+    const installation = '123'
+    const secret = crypto.createHmac('sha256', env.NPMHOOKS_SECRET)
+    .update(installation)
+    .digest('hex')
+
+    const hmacPayload = crypto.createHmac('sha256', secret)
+    .update(reqPayload)
+    .digest('hex')
+
+    const {statusCode} = await server.inject({
+      method: 'POST',
+      url: `/npm/${installation}`,
+      headers: {
+        'x-npm-signature': `sha256=${hmacPayload}`,
+        'Content-Type': 'application/json',
+        'Content-Length': payloadSize
       },
       payload: reqPayload
     })
 
-    t.is(statusCode, 202, 'statusCode')
+    t.true((payloadSize > 1024 * 1024 * 25), 'payload is bigger then 25MB')
+    t.is(statusCode, 413, 'statusCode')
     t.end()
   })
 })()
